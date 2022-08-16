2763a2407ddaa19fcc8ea1e10840fdd4bdff2099
Martha Mandizvidza <martha.mandizvidza@vodafone.com>
2022-08-16 20:58:10 +0100

    added focus for accessibility

--- src/App.js
+++ src/App.js
@@ -1,51 +1,99 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
 import List from './List'
 
+const ListTodoWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+`
+const ListTodoContainer = styled.div`
+	margin-top: 50px;
+	width: 80vw;
+	background: white;
+	text-align: center;
+	border-radius: 6px;
+`
+
+const StyledForm = styled.form`
+	padding-top: 20px;
+	margin: 0 auto;
+`
+
+const StyledInput = styled.input`
+	width: 300px;
+	height: 2rem;
+	text-transform: uppercase;
+
+	&:focus {
+		outline: 2px solid red;
+	}
+`
+const StyledButton = styled.button`
+	padding: 1rem 1rem;
+	border-radius: 6px;
+	background: black;
+	color: white;
+	outline: none;
+	border: 1px solid white;
+	margin-left: 10px;
+
+	&:focus {
+		outline: 2px solid red;
+	}
+`
+
 export default function App() {
- const [todo, setTodo] = useState([{ text: '', complete: false, id: 1}])
- const [todos, setTodos] = useState([])
+	const [todo, setTodo] = useState([{ text: '', complete: false, id: 1 }])
+	const [todos, setTodos] = useState([])
 
+	console.log('todo', todo)
 
- console.log('todo', todo)
+	const onChangeHandler = (e) => {
+		const idValue = Math.random()
+		setTodo({ text: e.target.value, id: idValue, complete: false })
+	}
+	const handleComplete = (id) => {
+		const newState = todos.map((obj) => {
+			// 👇️ if id equals 2, update country property
+			if (obj.id === id) {
+				return { ...obj, complete: true }
+			}
 
- const onChangeHandler = (e) => {
-	const idValue = Math.random()
-	setTodo({text: e.target.value, id: idValue, complete:false})
+			// 👇️ otherwise return object as is
+			return obj
+		})
 
- }
- const handleComplete = (id) => {
-	const newState = todos.map(obj => {
-		// 👇️ if id equals 2, update country property
-		if (obj.id === id) {
-		  return {...obj, complete: true};
-		}
-  
-		// 👇️ otherwise return object as is
-		return obj;
-	  });
-  
-	  setTodos(newState);
-	
-}
+		setTodos(newState)
+	}
 
-const deleteTodo = (id) => {
- const filtering = todos.filter(todo => todo.id !== id)
- console.log('filtered result', filtering)
- setTodos(filtering)
-}
- 
- const updateHandler = (e) => {
-	e.preventDefault()
-	setTodos(todos.concat(todo))
-	setTodo('')
-	console.log('Todos', todos)
- }
-  return (
-	<div>
-	 <form onSubmit={updateHandler}>
-		<input value={todo.text} onChange={onChangeHandler} placeholder='Search Item' />	
-	 </form>
-	 <List todos={todos} handleComplete={handleComplete} deleteTodo={deleteTodo}/>
-	</div>
-  )
+	const deleteTodo = (id) => {
+		const filtering = todos.filter((todo) => todo.id !== id)
+		console.log('filtered result', filtering)
+		setTodos(filtering)
+	}
+
+	const updateHandler = (e) => {
+		e.preventDefault()
+		setTodos(todos.concat(todo))
+		setTodo([{ ...todo, text: '' }])
+	}
+	return (
+		<ListTodoWrapper>
+			<ListTodoContainer>
+				<StyledForm onSubmit={updateHandler}>
+					<StyledInput
+						value={todo.text}
+						onChange={onChangeHandler}
+						placeholder='Search Item'
+					/>
+					<StyledButton onClick={updateHandler}>ADD</StyledButton>
+				</StyledForm>
+				<List
+					todos={todos}
+					handleComplete={handleComplete}
+					deleteTodo={deleteTodo}
+				/>
+			</ListTodoContainer>
+		</ListTodoWrapper>
+	)
 }
--- src/List.js
+++ src/List.js
@@ -1,15 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export default function List({todos, handleComplete, deleteTodo}) {
-  return (
-    <div>
-      {todos.map((todo, index) => {
-       return (
-        <>
-        <p key={index}>{todo.text}  <span onClick={() => {handleComplete(todo.id)}}>{todo.complete === false ? 'incomplete' : 'complete'}</span ><span onClick={() => {deleteTodo(todo.id)}}>Delete</span></p>
-        </>
-       )
-      })}
-    </div>
-  )
+const ListWrapper = styled.div`
+	width: 80vw;
+	height: 60vh;
+	background: white;
+	color: black;
+	text-transform: uppercase;
+`
+const StyledButton = styled.button`
+	background: black;
+	color: white;
+	margin-left: 2rem;
+	padding: 1rem 2rem;
+	border: 1px solid grey;
+
+	&:focus {
+		outline: 2px solid red;
+	}
+`
+export default function List({ todos, handleComplete, deleteTodo }) {
+	return (
+		<ListWrapper>
+			{todos.map((todo, index) => {
+				return (
+					<>
+						<p key={index}>
+							{todo.text}{' '}
+							<StyledButton
+								onClick={() => {
+									handleComplete(todo.id)
+								}}
+							>
+								{todo.complete === false ? 'incomplete' : 'complete'}
+							</StyledButton>
+							<StyledButton
+								onClick={() => {
+									deleteTodo(todo.id)
+								}}
+							>
+								Delete
+							</StyledButton>
+						</p>
+					</>
+				)
+			})}
+		</ListWrapper>
+	)
 }
